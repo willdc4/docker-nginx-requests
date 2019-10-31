c8a2f9999d5e7d97f55b67739ad8ac3d3fca056f
William Coelho <35871638+willdc4@users.noreply.github.com>
2019-10-31 19:17:22 -0300

    Update server.js

--- valida-cpf/server.js
+++ valida-cpf/server.js
@@ -6,6 +6,9 @@ const validarCpf = require('validar-cpf');
 // Constants
 const PORT = 8080;
 
+//ID
+const id = Math.floor(Math.random() * 10000);
+
 // App
 const app = express();
 app.get('/', (req, res) => {
@@ -16,6 +19,9 @@ app.get('/validar/:cpf', (req, res) => {
     var cpf = req.params.cpf;
     res.send(validarCpf(cpf)+'\n');
 });
-  
+
+app.get('/id/', (req, res) => {
+     res.send('ID: ' + id +'\n');
+});  
 app.listen(PORT);
 console.log(`Running listening on port ${PORT}`);
